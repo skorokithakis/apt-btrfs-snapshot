eabc9cb5ff26b866b8fae0308d1284001debc76c
Michael Vogt <michael.vogt@ubuntu.com>
2011-07-19 13:42:45 +0200

    * use argparse instead of home grown parser and add help * add i18n

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from DistUtilsExtra.command import build_i18n, build_extra
 
 setup(name='apt-btrfs-snapshot',
       version='0.1',
@@ -8,4 +9,8 @@ setup(name='apt-btrfs-snapshot',
       url='http://launchpad.net/apt-btrfs-snapshot',
       scripts=['apt-btrfs-snapshot'],
       py_modules=['apt_btrfs_snapshot'],
+      cmdclass = { 
+        "build" : build_extra.build_extra,
+        "build_i18n" : build_i18n.build_i18n,
+        }
       )
